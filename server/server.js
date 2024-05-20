d9d9adfed82d648582fdf752fc09f1a795c00753
REBUG2416 <davidagochukwu@gmail.com>
2024-05-20 15:17:56 +0100

    New commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors'); // Import the cors module
 const sql = require('msnodesqlv8'); // Import the mssql module
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors()); 
@@ -84,6 +85,6 @@ app.post("/api/notes", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server started on port 5000");
 });
