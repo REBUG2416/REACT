54347f03b7998eafefbbfb3021fb71c051dfd97f
REBUG2416 <davidagochukwu@gmail.com>
2024-05-23 14:43:06 +0100

    New commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors'); // Import the cors module
-/* const sql = require('msnodesqlv8'); // Import the mssql module
+/* const sql = require('msnodesqlv8'); // Import the mssql module*/
 const port = process.env.PORT || 5000;
- */
+
 const app = express();
 app.use(cors()); 
 app.use(express.json()); 
