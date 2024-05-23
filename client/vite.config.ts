d6d18b6c807d5dbf7d325cd882c6601c04409a7a
REBUG2416 <davidagochukwu@gmail.com>
2024-05-23 21:33:41 +0100

    New commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     outDir: "dist",
   },
   server: {
-    port: parseInt(process.env.PORT) || 5000,
+    port: Number(process.env.PORT) || 5000,
   },
   optimizeDeps: {
     include: ["react", "react-dom"],
